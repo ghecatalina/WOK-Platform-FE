e17fa24a54cf442862d141f897de60f34bde8f09
ghecatalina <gheorghioiu.catalina@gmail.com>
2023-06-22 21:18:49 +0300

    removed key with env var

--- wok-platform-fe/src/pages/Contact/GoogleMaps.js
+++ wok-platform-fe/src/pages/Contact/GoogleMaps.js
@@ -15,7 +15,7 @@ const center = {
 function MyComponent() {
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
-    googleMapsApiKey: //Google_Maps_Key
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
   })
 
   const [map, setMap] = React.useState(null)
